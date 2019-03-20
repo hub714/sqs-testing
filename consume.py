363d776137a4e0a002b838e18dc5be809c41ad6d
hubertc <>
2019-03-19 21:28:10 -0700

    ha

--- consume.py
+++ consume.py
@@ -1,5 +1,6 @@
 import boto3
 import threading
+import time
 
 sqs = boto3.resource('sqs', region_name='us-west-2')
 queue = sqs.get_queue_by_name(QueueName='tps-test')
@@ -9,5 +10,6 @@ def loop():
     #  print(message)
         print(message.body)
         time.sleep(0.2)
+        print(message.delete())
 
 threading.Thread(target=loop).start()
