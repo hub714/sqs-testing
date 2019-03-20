4df67e168144ad088c927bd1230f5ca58c924133
hubertc <>
2019-03-19 21:38:17 -0700

    10 threads in publisher

--- publish.py
+++ publish.py
@@ -1,8 +1,21 @@
 import boto3
+import threading
 # Get the service resource
 sqs = boto3.resource('sqs', region_name='us-west-2')
 # get queue
 queue = sqs.get_queue_by_name(QueueName='tps-test')
 
-for x in range(10000):
-  response = queue.send_message(MessageBody=str(x))
+def loop:
+    for x in range(10000):
+        response = queue.send_message(MessageBody=str(x))
+
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
+threading.Thread(target=loop).start()
